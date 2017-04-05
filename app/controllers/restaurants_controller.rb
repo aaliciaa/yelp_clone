75330610db9a5bd0991a355f712831fc38516c01
Alicia Bolton <alicia.bolton@hotmail.com>
2017-04-05 12:03:03 +0900

    Added gem files

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,26 @@
+class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+
+end
